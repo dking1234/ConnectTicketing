5ab137a18ff6fef9b34c22ebd25da44990f8a06d
dking1234 <39795989+dking1234@users.noreply.github.com>
2023-12-13 17:46:54 +0800

    package.json react-native-navigation

--- Screens/Ticket.js
+++ Screens/Ticket.js
@@ -1,28 +1,81 @@
-import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
 
 const Ticket = () => {
+  const [tickets, setTickets] = useState([]);
+  const [activeFilter, setActiveFilter] = useState(true);
+
+  useEffect(() => {
+    // Fetch userId from AsyncStorage
+    const fetchUserId = async () => {
+      try {
+        const userId = await AsyncStorage.getItem('userId');
+        if (userId) {
+          // Fetch tickets based on userId
+          fetchTickets(userId);
+        }
+      } catch (error) {
+        console.error('Error fetching userId from AsyncStorage:', error);
+      }
+    };
+
+    fetchUserId();
+  }, []); // Empty dependency array to ensure this effect runs only once on mount
+
+  const fetchTickets = async (userId) => {
+    try {
+      const response = await axios.get(`http://192.168.43.21:80/api/tickets/user/${userId}`);
+      setTickets(response.data);
+    } catch (error) {
+      console.error('Error fetching tickets:', error);
+    }
+  };
+
+  const filteredTickets = () => {
+    if (activeFilter) {
+      return tickets.filter((ticket) => ticket.status === 'active');
+    } else {
+      return tickets.filter((ticket) => ticket.status === 'expired');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      
       <View style={styles.header}>
         <Text style={styles.headerText}>My Tickets</Text>
-        {/* Add your icon here if you have one */}
       </View>
-      
+
       <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.activeButton}>
+        <TouchableOpacity
+          style={[styles.filterButton, activeFilter ? styles.activeButton : styles.expiredButton]}
+          onPress={() => setActiveFilter(true)}
+        >
           <Text style={styles.buttonText}>Active</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.expiredButton}>
+        <TouchableOpacity
+          style={[styles.filterButton, activeFilter ? styles.expiredButton : styles.activeButton]}
+          onPress={() => setActiveFilter(false)}
+        >
           <Text style={styles.buttonText}>Expired</Text>
         </TouchableOpacity>
       </View>
-<View style={{justifyContent: 'center', marginTop: 200}}>
-      <Image source={require('../Images/Tickets.png')} style={styles.ticketIcon} />
 
-      <Text style={styles.message}>You don’t have any active tickets</Text>
-      </View>
+      {filteredTickets().length > 0 ? (
+        // Render your list of filtered tickets here
+        // Example: map through filteredTickets and display ticket information
+        <View>
+          {filteredTickets().map((ticket) => (
+            <Text key={ticket.id}></Text>
+          ))}
+        </View>
+      ) : (
+        <View style={{ justifyContent: 'center', marginTop: 200 }}>
+          <Image source={require('../Images/Tickets.png')} style={styles.ticketIcon} />
+          <Text style={styles.message}>You don’t have any {activeFilter ? 'active' : 'expired'} tickets</Text>
+        </View>
+      )}
     </View>
   );
 };
@@ -52,18 +105,17 @@ const styles = StyleSheet.create({
     marginBottom: 40,
     padding: 20,
   },
-  activeButton: {
+  filterButton: {
     flex: 1,
     padding: 15,
-    backgroundColor: '#FF7927',
-    marginRight: 10,
     borderRadius: 10,
   },
+  activeButton: {
+    backgroundColor: '#FF7927',
+    marginRight: 10,
+  },
   expiredButton: {
-    flex: 1,
-    padding: 15,
     backgroundColor: 'lightgray',
-    borderRadius: 10,
   },
   buttonText: {
     textAlign: 'center',
@@ -82,4 +134,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Ticket
+export default Ticket;
